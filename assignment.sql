5210e63e96715f9ba47cac5e8604631a8e9c6e8a
Kleng924 <168580002+Kleng924@users.noreply.github.com>
2024-08-05 17:29:12 -0700

    Add files via upload

--- assignment.sql
+++ assignment.sql
@@ -1,3 +1,11 @@
 USE mysql_assignment_bd;
 SELECT * from Members;
 
+INSERT INTO Members (id, name, age) VALUES
+(1, 'Jane Doe', 28),
+(2, 'John Smith', 35),
+(3, 'Alice Johnson', 22);
+
+DELETE FROM Members
+WHERE name = 'John Smith';
+
